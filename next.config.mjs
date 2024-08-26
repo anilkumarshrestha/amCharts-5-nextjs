58b763e7cdab5e94cef1143fbc8a280efbf0064d
Anil Kumar Shrestha <anilkumarshrestha@yahoo.com>
2024-08-26 18:49:21 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
+export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/amCharts-5-nextjs",
-  output: "export",
+  // basePath: "/amCharts-5-nextjs",
+  output:'export',
 };
 
-module.exports = nextConfig
+export default nextConfig;
